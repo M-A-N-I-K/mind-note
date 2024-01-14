8005ad70a948467f6fbd4c46f8759674ab9ba399
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-01-14 23:20:01 +0530

    marketing page with footer added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+	return <div className="bg-red-400 text-white h-full">{children}</div>;
+};
+
+export default RootLayout;
---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+
+export const Footer = () => {
+	return (
+		<div className="flex items-center w-full p-6 bg-background z-50">
+			<Logo />
+			<div className="flex md:justify-end justify-between items-center md:ml-auto w-full gap-x-2 text-muted-foreground">
+				<Button variant="ghost" size="sm">
+					Privacy Policy
+				</Button>
+				<Button variant="ghost" size="sm">
+					Terms & Conditions
+				</Button>
+			</div>
+		</div>
+	);
+};
