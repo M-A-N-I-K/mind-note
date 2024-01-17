d37709918ccad0d0cc45fa8eedb615823132aa0a
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-01-17 19:12:40 +0530

    authentication added through convex and clerk

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./logo";
 
 export const Footer = () => {
 	return (
-		<div className="flex items-center w-full p-6 bg-background z-50">
+		<div className="flex items-center w-full p-6 bg-background dark:bg-[#1F1F1F] z-50">
 			<Logo />
 			<div className="flex md:justify-end justify-between items-center md:ml-auto w-full gap-x-2 text-muted-foreground">
 				<Button variant="ghost" size="sm">
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,20 +1,37 @@
 "use client";
 import useScrollTop from "@/hooks/use-scroll-top";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
 import { ModeToggle } from "@/components/modeToggle";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
 	const scrolled = useScrollTop();
 	return (
 		<div
 			className={cn(
-				"z-50 bg-background fixed top-0 flex items-center w-full p-6",
+				"z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
 				scrolled && "border-b shadow-sm"
 			)}
 		>
 			<Logo />
 			<div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+				{isLoading && <p>Loading...</p>}
+				{!isAuthenticated && !isLoading && (
+					<>
+						<SignInButton mode="modal">
+							<Button variant="ghost" size="sm">
+								Log in
+							</Button>
+						</SignInButton>
+						<SignInButton mode="modal">
+							<Button size="sm">Get Motion Free</Button>
+						</SignInButton>
+					</>
+				)}
 				<ModeToggle />
 			</div>
 		</div>
