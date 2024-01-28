acb74456b4b00340709d12361d1e584a2d2e6778
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-01-29 00:52:48 +0530

    query to get documents from table added

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
+
+import { cn } from "@/lib/utils";
 import Useritem from "./UserItem";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export const Navigation = () => {
 	const pathname = usePathname();
 	const isMobile = useMediaQuery("(max-width: 768px)");
+	const documents = useQuery(api.documents.get);
 
 	const isResizingRef = useRef(false);
 	const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -117,7 +122,9 @@ export const Navigation = () => {
 					<Useritem />
 				</div>
 				<div className="mt-4">
-					<p>Documents</p>
+					{documents?.map((doc) => (
+						<p key={doc._id}>{doc.title}</p>
+					))}
 				</div>
 				<div
 					onMouseDown={handleMouseDown}
