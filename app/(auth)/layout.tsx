b6ed99c5af213439b610ef65a2f3e2228722dd0c
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-01-28 22:26:15 +0530

    sidebar added

--- app/(auth)/layout.tsx
+++
@@ -1,5 +0,0 @@
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div className="bg-red-400 text-white h-full">{children}</div>;
-};
-
-export default RootLayout;
---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,14 @@
+import { Navigation } from "../../_components/Navigation";
+
+const DocumentsPage = () => {
+	return (
+		<>
+			<Navigation />
+			<main className="h-full flex-1 overflow-y-auto">
+				This is a documents Page
+			</main>
+		</>
+	);
+};
+
+export default DocumentsPage;
