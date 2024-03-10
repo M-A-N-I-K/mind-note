41335304904107d3eafcdce7350922b1425e2657
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-03-11 04:47:03 +0530

    optimized code for replace image

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
 	url?: string;
@@ -19,8 +20,13 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
 	const coverImage = useCoverImage();
 	const params = useParams();
 	const removeCoverImage = useMutation(api.documents.removeCoverImage);
+	const { edgestore } = useEdgeStore();
 
 	const onRemove = async () => {
+		await edgestore.publicFiles.delete({
+			url,
+		});
+
 		await removeCoverImage({
 			id: params.documentId as Id<"documents">,
 		});
@@ -41,7 +47,7 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
 			{url && !preview && (
 				<div className="absolute flex items-center gap-x-2 bottom-5 right-5 bg-black/40 opacity-0 group-hover:opacity-100">
 					<Button
-						onClick={coverImage.onOpen}
+						onClick={() => coverImage.onReplace(url)}
 						className="text-muted-foreground text-xs"
 						variant="outline"
 						size="sm"
